6be2083bd15a9c2bdecdde577b5374ed05424d7b
Garv Jindal <garvjindal2@gmail.com>
2026-03-26 16:27:49 +0530

    Time: 9 ms (43.99%) | Memory: 23.3 MB (45.99%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -3,10 +3,7 @@ class Solution:
         d={}
         out=[]
         for x in nums:
-            if x in d:
-                d[x]+=1
-            else:
-                d[x]=1
+                d[x]=d.get(x,0)+1
         for (key,value) in d.items():
             if value>len(nums)/3:
                 out.append(key)
