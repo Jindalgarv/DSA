a71e83b5899ba483d6822b24075550fe3d8717a7
Garv Jindal <garvjindal2@gmail.com>
2026-04-10 22:42:33 +0530

    Time: 1 ms (46.07%) | Memory: 19.3 MB (76.20%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        mapping = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
-
-        for char in s:
-            if char in mapping:
-                if not stack or stack[-1] != mapping[char]:
+        stack=[]
+        mapping={')':'(','}':'{',']':'['}
+        for x in s:
+            if x in mapping:
+                if not stack or stack[-1]!=mapping[x]:
                     return False
-                stack.pop()
+                else:
+                    stack.pop()
             else:
-                stack.append(char)
-
-        return len(stack) == 0
+                stack.append(x)
+        return not stack
