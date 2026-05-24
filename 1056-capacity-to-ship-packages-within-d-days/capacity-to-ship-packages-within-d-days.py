fe83ce0829895034adf88f7892163362c2bfd969
Garv Jindal <garvjindal2@gmail.com>
2026-05-24 22:08:25 +0530

    Time: 181 ms (93.24%) | Memory: 23.8 MB (93.21%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -7,13 +7,13 @@ class Solution:
         while l<=r:
             mid=(l+r)//2
             s=0
-            day=0
+            day=1
             for x in weights:
                 s+=x
                 if s>mid:
                     s=x
                     day+=1
-            if day>=days:
+            if day>days:
                 l=mid+1
             else:
                 r=mid-1
