9afdf305a6ead1350f18c0faeff6036f6d70bb9f
Garv Jindal <garvjindal2@gmail.com>
2026-05-25 12:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.34%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,15 +1,27 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        ans= float('inf')
-        l,r=0,len(nums)-1
-        while l<=r:
+#striver version
+        # ans= float('inf')
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=l+(r-l)//2
+        #     #if left half is sorted
+        #     if(nums[l]<=nums[m]):
+        #         ans=min(ans,nums[l])
+        #         l=m+1
+        #     #if right half is sorted
+        #     elif(nums[m]<=nums[r]):
+        #         ans=min(ans,nums[m])
+        #         r=m-1
+        # return ans
+# GPT optimal version
+
+        l,r= 0,len(nums)-1
+        while l<r:
             m=l+(r-l)//2
-            #if left half is sorted
-            if(nums[l]<=nums[m]):
-                ans=min(ans,nums[l])
+            #if min lies right
+            if nums[m]>nums[r]:
                 l=m+1
-            #if right half is sorted
-            elif(nums[m]<=nums[r]):
-                ans=min(ans,nums[m])
-                r=m-1
-        return ans
+            else:
+                r=m
+        return nums[l]
