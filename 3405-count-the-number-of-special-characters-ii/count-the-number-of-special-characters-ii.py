c4cc4ca8489a2d26b25e442109808c9e17bdea8c
Garv Jindal <garvjindal2@gmail.com>
2026-05-27 18:48:46 +0530

    Time: 255 ms (58.82%) | Memory: 21.7 MB (28.82%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,8 +1,22 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        count=0
-        for i in range(0,len(word)):
-            if chr(ord(word[i])+32) in word[:i] and chr(ord(word[i])+32) not in word[i+1:] and word[i] not in word[:i]:
-                count+=1
-        return count
+        # count=0
+        # for i in range(0,len(word)):
+        #     if chr(ord(word[i])+32) in word[:i] and chr(ord(word[i])+32) not in word[i+1:] and word[i] not in word[:i]:
+        #         count+=1
+        # return count
+        idxl=[-1]*26
+        idxu=[-1]*26
+        for i,x in enumerate(word):
+            if x.islower():
+                idxl[ord(x)-ord('a')]=i
+            elif idxu[ord(x)-ord('A')]==-1:
+                idxu[ord(x)-ord('A')]=i
+        ans=0
+        for i in range(26):
+            if idxl[i]>=0 and idxu[i]>=idxl[i]:
+                ans+=1
+        return ans
+
+
         
