398d068e26adaae0c7f7bc89ee79c92ee669521b
Garv Jindal <garvjindal2@gmail.com>
2026-06-14 13:40:06 +0530

    Time: 30 ms (73.44%) | Memory: 21 MB (62.55%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        freq=Counter(hand)
+        for num in sorted(freq):
+            while freq[num]>0:
+                for nxt in range(num,num+groupSize):
+                    if freq[nxt]==0:
+                        return False
+                    else:
+                        freq[nxt]-=1
+        return True
+    
