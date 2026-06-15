49e98832acb687cd63384cedec029b4533994912
Garv Jindal <garvjindal2@gmail.com>
2026-06-15 15:34:29 +0530

    Time: 147 ms (86.20%) | Memory: 42 MB (57.73%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,19 +1,31 @@
 class MedianFinder:
 
     def __init__(self):
-        self.arr=[]
+        self.minheap=[]
+        self.maxheap=[]
 
     def addNum(self, num: int) -> None:
-        self.arr.append(num)
+        if len(self.minheap)==len(self.maxheap)==0:
+            heappush(self.minheap,num)
+        elif len(self.minheap)==len(self.maxheap):
+            if num<(-self.maxheap[0]):
+                heappush(self.minheap,-heappop(self.maxheap))
+                heappush(self.maxheap,-num)
+            else: heappush(self.minheap,num)
+        else:
+            if num>=self.minheap[0]:
+                heappush(self.maxheap,-heappop(self.minheap))
+                heappush(self.minheap,num)
+            else:
+                heappush(self.maxheap,-num)
 
     def findMedian(self) -> float:
-        if not self.arr:
-            return False
-        self.arr.sort()
-        n=len(self.arr)
-        if n%2==0:
-            return (self.arr[n//2-1]+self.arr[n//2])/2
-        return self.arr[(n-1)//2]
+        if not self.minheap and not self.maxheap:
+            return []
+        elif len(self.minheap)==len(self.maxheap):
+            return (self.minheap[0]-self.maxheap[0])/2
+        else:
+            return self.minheap[0]
 
 
 
