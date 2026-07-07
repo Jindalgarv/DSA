219c21cb1cfc267ff27179b73330472bd1044aa5
Garv Jindal <garvjindal2@gmail.com>
2026-07-07 14:27:48 +0530

    Time: 149 ms (64.66%) | Memory: 19.7 MB (76.47%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -6,15 +6,15 @@ class Solution:
         directions=((1,1),(1,0),(0,1),(1,-1),(-1,1),(-1,-1),(0,-1),(-1,0))
         distance=[[float('inf')]*n for _ in range(n)]
         distance[0][0]=0
-        pq=[]
-        heappush(pq,(0,0,0))
-        while pq:
-            dis,r,c=heappop(pq)
+        q=deque()
+        q.append((0,0,0))
+        while q:
+            dis,r,c=q.popleft()
             for dr,dc in directions:
                 nr,nc=r+dr,c+dc
                 if 0<=nr<n and 0<=nc<n and not grid[nr][nc] and distance[nr][nc]>1+dis:
                     distance[nr][nc]=1+dis
-                    heappush(pq,(dis+1,nr,nc))
+                    q.append((dis+1,nr,nc))
         if distance[n-1][n-1]<float('inf'):
             return distance[n-1][n-1]+1
         return -1
