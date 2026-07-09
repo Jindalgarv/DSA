88af5558042e7c77dd18fab77f62e32ece0b2361
Garv Jindal <garvjindal2@gmail.com>
2026-07-10 04:01:45 +0530

    Time: 8 ms (22.36%) | Memory: 21 MB (39.15%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,23 +1,25 @@
 class Solution:
+    #basically have to check cycle only if it contains cycle then return False
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph=[[] for _ in range(numCourses)]
         for course, prereq in prerequisites:
             graph[prereq].append(course)
-        state=[0]*numCourses
+        visited=[0]*numCourses
         def dfs(node):
-            if state[node]==1:
+            if visited[node]==1:
                 return True
-            if state[node]==2:
+            if visited[node]==2:
                 return False
-            state[node]=1
+            visited[node]=1
             for neighbour in graph[node]:
                 if dfs(neighbour):
                     return True
-            state[node]=2
-            return False
-        for course in range(numCourses):
-            if not state[course]:
-                if dfs(course):
+            visited[node]=2
+            
+        for i in range(numCourses):
+            if not visited[i]:
+                if dfs(i):
                     return False
         return True
-        
+            
+                
