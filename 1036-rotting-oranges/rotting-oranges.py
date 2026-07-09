f2faad27f0daba9f2e9fbae7bda6bc8356889e8d
Garv Jindal <garvjindal2@gmail.com>
2026-07-10 03:37:25 +0530

    Time: 4 ms (37.26%) | Memory: 19.4 MB (12.52%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,41 +1,28 @@
 from collections import deque
-
+from typing import List
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
+        time=0
+        directions=((0,1),(0,-1),(1,0),(-1,0))
+        q=deque()
+        m,n= len(grid),len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==2:
+                    q.append((i,j,time))
+        while q:
+            row,col,t=q.popleft()
+            time=t
+            for dr,dc in directions:
+                nr,nc=row+dr,col+dc
+                if 0<=nr<m and 0<=nc<n and grid[nr][nc]==1:
+                    grid[nr][nc]=2
+                    q.append((nr,nc,t+1))
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    return -1
+        return time
 
-        q = deque()
-        fresh = 0
 
-        # Add all rotten oranges to the queue and count fresh oranges
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    q.append((r, c))
-                elif grid[r][c] == 1:
-                    fresh += 1
 
-        # (up, right, down, left)
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-        time = 0
-
-        while q and fresh:
-            for _ in range(len(q)):          # Process one level (one minute)
-                r, c = q.popleft()
-
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-
-                    if (0 <= nr < rows and
-                        0 <= nc < cols and
-                        grid[nr][nc] == 1):
-
-                        grid[nr][nc] = 2      # Mark as rotten
-                        fresh -= 1
-                        q.append((nr, nc))
-
-            time += 1
-
-        return time if fresh == 0 else -1
-    
