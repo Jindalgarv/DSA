c5ccf0a550646bbe7bfe7e36a9c226c8f5a7d930
Garv Jindal <garvjindal2@gmail.com>
2026-07-13 02:24:42 +0530

    Time: 3 ms (20.10%) | Memory: 19.6 MB (11.57%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -6,4 +6,15 @@
 from math import comb
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return comb(m+n-2,m-1)
+        dp=[[-1]*n for _ in range(m)]
+        dp[0][0]=1
+        def solve(i,j):
+            if i<0 or j<0:
+                return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            up=solve(i-1,j)
+            left=solve(i,j-1)
+            dp[i][j]=solve(i-1,j)+ solve(i,j-1)
+            return dp[i][j]
+        return solve(m-1,n-1)
