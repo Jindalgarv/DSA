5aea65b57c08ce8f1645fff9021642d4c265b0d3
Garv Jindal <garvjindal2@gmail.com>
2026-07-13 03:56:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (31.92%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,17 +1,35 @@
 class Solution:
+
+    # =====================================================
+    # SPACE OPTIMIZED DP
+    # Time  : O(m*n)
+    # Space : O(n)
+    # =====================================================
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m,n=len(obstacleGrid),len(obstacleGrid[0])
-        prev=[0]*n
+
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+
+        prev = [0] * n
+
         for i in range(m):
-            curr=[0]*n
+
+            curr = [0] * n
+
             for j in range(n):
+
+                # Current cell is blocked
                 if obstacleGrid[i][j]:
-                    curr[j]=0
-                elif i==0 and j==0:
-                    curr[j]=1
+                    curr[j] = 0
+
+                # Starting cell
+                elif i == 0 and j == 0:
+                    curr[j] = 1
+
                 else:
-                    up=prev[j]
-                    left=curr[j-1] if j>0 else 0
-                    curr[j]=up+left
-            prev=curr
+                    up = prev[j]
+                    left = curr[j - 1] if j > 0 else 0
+                    curr[j] = up + left
+
+            prev = curr
+
         return prev[-1]
