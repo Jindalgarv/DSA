6126a5eef046f543e7650c8049cf2edadfd08684
Garv Jindal <garvjindal2@gmail.com>
2026-07-12 19:07:03 +0530

    Time: 2 ms (24.20%) | Memory: 19.4 MB (41.64%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,4 +1,9 @@
-from math import factorial
+# from math import factorial
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         return factorial(m+n-2)//(factorial(m-1)*factorial(n-1))
+
+from math import comb
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return factorial(m+n-2)//(factorial(m-1)*factorial(n-1))
+        return comb(m+n-2,m-1)
