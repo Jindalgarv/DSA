8ac50fcbb49889c37d1444d210af06e78f0a70c6
Garv Jindal <garvjindal2@gmail.com>
2026-07-14 15:42:47 +0530

    Time: 6 ms (95.38%) | Memory: 20.2 MB (99.80%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,19 +1,19 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         #RECURSIVE SOLUTION with MEMOIZATION
-        m,n=len(grid),len(grid[0])
-        dp=[[-1]*n for _ in range(m)]
-        def solve(i,j):
-            if i==0 and j==0:
-                return grid[0][0]
-            elif i<0 or j<0:
-                return float('inf')
-            elif dp[i][j]!=-1:
-                return dp[i][j]
-            else:
-                dp[i][j]=min(solve(i-1,j),solve(i,j-1))+grid[i][j]
-                return dp[i][j]
-        return solve(m-1,n-1)
+        # m,n=len(grid),len(grid[0])
+        # dp=[[-1]*n for _ in range(m)]
+        # def solve(i,j):
+        #     if i==0 and j==0:
+        #         return grid[0][0]
+        #     elif i<0 or j<0:
+        #         return float('inf')
+        #     elif dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     else:
+        #         dp[i][j]=min(solve(i-1,j),solve(i,j-1))+grid[i][j]
+        #         return dp[i][j]
+        # return solve(m-1,n-1)
         
 #TABULATION SOLUTION
 #         m,n=len(grid),len(grid[0])
@@ -34,16 +34,20 @@ class Solution:
 
 #TABULATION WITH SPACE OPTIMIZATION
 
-        m,n=len(grid),len(grid[0])
-        prev=[float('inf')]*n
-        for i in range(1,m):
-            curr=[float('inf')]*n
-            curr[0]=grid[i][0]
-            for j in range(1,n):
-                curr[i][j]=min(curr[j-1],prev[j])+grid[i][j]
-            prev=curr
-        return curr[m-1][n-1]
-        
+        m, n = len(grid), len(grid[0])
+        prev = [0] * n
 
+        for i in range(m):
+            curr = [0] * n
+            for j in range(n):
+                if i == 0 and j == 0:
+                    curr[j] = grid[0][0]
+                elif i == 0:
+                    curr[j] = curr[j - 1] + grid[i][j]
+                elif j == 0:
+                    curr[j] = prev[j] + grid[i][j]
+                else:
+                    curr[j] = min(prev[j], curr[j - 1]) + grid[i][j]
+            prev = curr
 
-
+        return prev[-1]
