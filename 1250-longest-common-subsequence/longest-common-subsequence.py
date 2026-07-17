406879197e83e39aac95f0cbbc49708fef5f78e1
Garv Jindal <garvjindal2@gmail.com>
2026-07-17 20:30:49 +0530

    Time: 356 ms (57.73%) | Memory: 44.4 MB (58.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -27,27 +27,44 @@
         # return solve(n1-1,n2-1)
 #WHAT CHATGPT RECOMMENDED CHANGES BASICALLY IN THE BASE CASES
 
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # n1, n2 = len(text1), len(text2)
+
+        # dp = [[-1] * n2 for _ in range(n1)]
+
+        # def solve(i, j):
+        #     if i < 0 or j < 0:
+        #         return 0
+
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+
+        #     if text1[i] == text2[j]:
+        #         dp[i][j] = 1 + solve(i - 1, j - 1)
+        #     else:
+        #         dp[i][j] = max(solve(i - 1, j), solve(i, j - 1))
+
+        #     return dp[i][j]
+
+        # return solve(n1 - 1, n2 - 1)
+
+#TABULATION(INSTEAD OF TAKING DP ARRAY OF N1*N2 I WILL TAKE N1+1 * N2+1 SO IF ANY STRING OF 1 SIZE IS GIVEN IT WILL TAKE CARE OF IT)
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1, n2 = len(text1), len(text2)
 
-        dp = [[-1] * n2 for _ in range(n1)]
+        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
 
-        def solve(i, j):
-            if i < 0 or j < 0:
-                return 0
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-            if dp[i][j] != -1:
-                return dp[i][j]
-
-            if text1[i] == text2[j]:
-                dp[i][j] = 1 + solve(i - 1, j - 1)
-            else:
-                dp[i][j] = max(solve(i - 1, j), solve(i, j - 1))
-
-            return dp[i][j]
-
-        return solve(n1 - 1, n2 - 1)
+        return dp[n1][n2]
+        
 
 
 
