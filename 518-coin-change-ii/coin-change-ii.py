fd3a0c27fc9c24537218466583c196f990aca7c7
Garv Jindal <garvjindal2@gmail.com>
2026-07-17 18:08:36 +0530

    Time: 407 ms (48.95%) | Memory: 62.3 MB (38.85%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n=len(coins)
+        dp=[[-1]*(amount+1) for _ in range(n)]
+        def solve(i,target):
+            if amount==0:
+                return 1
+            if i==0:
+                if not target%coins[i]:
+                    return 1
+                return 0
+            if dp[i][target]!=-1:
+                return dp[i][target]
+            not_taken=solve(i-1,target)
+            taken=0
+            if coins[i]<=target:
+                taken=solve(i,target-coins[i])
+            dp[i][target]=taken+not_taken
+            return dp[i][target]
+        return solve(n-1,amount)
+
