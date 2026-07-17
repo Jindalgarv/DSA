f3c9cea11ced84df2e9d02525004ba6783964889
Garv Jindal <garvjindal2@gmail.com>
2026-07-18 02:33:42 +0530

    Time: 263 ms (92.14%) | Memory: 19.3 MB (97.52%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -50,21 +50,40 @@
         # return solve(n1 - 1, n2 - 1)
 
 #TABULATION(INSTEAD OF TAKING DP ARRAY OF N1*N2 I WILL TAKE N1+1 * N2+1 SO IF ANY STRING OF 1 SIZE IS GIVEN IT WILL TAKE CARE OF IT)
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         n1, n2 = len(text1), len(text2)
+
+#         dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+
+
+#         for i in range(1, n1 + 1):
+#             for j in range(1, n2 + 1):
+#                 if text1[i - 1] == text2[j - 1]:
+#                     dp[i][j] = 1 + dp[i - 1][j - 1]
+
+#                 else:
+#                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+#         return dp[n1][n2]
+    
+#SPACE OPTIMIZED
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1, n2 = len(text1), len(text2)
 
-        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+        prev = [0] * (n2 + 1)
 
 
         for i in range(1, n1 + 1):
+            curr = [0] * (n2 + 1)
             for j in range(1, n2 + 1):
                 if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                    curr[j] = 1 + prev[j - 1]
 
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        return dp[n1][n2]
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev=curr
+        return prev[n2]
     
         
 
