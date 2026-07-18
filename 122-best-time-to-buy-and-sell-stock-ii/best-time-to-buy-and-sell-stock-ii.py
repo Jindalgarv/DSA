3fa2228a58e4657dbd612b769db8f257688a735c
Garv Jindal <garvjindal2@gmail.com>
2026-07-19 03:31:46 +0530

    Time: 19 ms (7.99%) | Memory: 23.2 MB (5.79%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -7,22 +7,36 @@
         # return profit
 
 #DP SOLUTION 
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         n=len(prices)
+#         dp=[[-1]*2 for _ in range(n)]
+#         profit=0
+#         @cache
+#         def solve(i,buy):
+#             if i==n:
+#                 return 0
+#             if dp[i][buy]!=-1:
+#                 return dp[i][buy]
+#             if buy:
+#                 profit=max(-prices[i]+solve(i+1,0),solve(i+1,1))
+#             else:
+#                 profit=max(prices[i]+solve(i+1,1),solve(i+1,0))
+#             dp[i][buy]=profit
+#             return dp[i][buy]
+#         return solve(0,1)
+
+#TABULATION CODE 
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        #dp=[[-1]*2 for _ in range(n)]
-        profit=0
-        @cache
-        def solve(i,buy):
-            if i==n:
-                return 0
-            # if dp[i][buy]!=-1:
-            #     return dp[i][buy]
-            if buy:
-                profit=max(-prices[i]+solve(i+1,0),solve(i+1,1))
-            else:
-                profit=max(prices[i]+solve(i+1,1),solve(i+1,0))
-            #dp[i][buy]=profit
-            #return dp[i][buy]
-            return profit
-        return solve(0,1)
+        dp=[[0]*2 for _ in range(n+1)]
+
+        for i in range(n-1,-1,-1):
+            dp[i][1]=max(-prices[i]+dp[i+1][0],dp[i+1][1])
+            dp[i][0]=max(prices[i]+dp[i+1][1],dp[i+1][0])
+        return dp[0][1]
+                
+
+
