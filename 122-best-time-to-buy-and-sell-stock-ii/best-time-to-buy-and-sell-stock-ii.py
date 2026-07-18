85325a6b0b192b690f9b1296bd2230c108e34b9c
Garv Jindal <garvjindal2@gmail.com>
2026-07-19 03:35:20 +0530

    Time: 11 ms (10.24%) | Memory: 20.4 MB (51.30%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -28,15 +28,28 @@
 
 #TABULATION CODE 
 
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         n=len(prices)
+#         dp=[[0]*2 for _ in range(n+1)]
+
+#         for i in range(n-1,-1,-1):
+#             dp[i][1]=max(-prices[i]+dp[i+1][0],dp[i+1][1])
+#             dp[i][0]=max(prices[i]+dp[i+1][1],dp[i+1][0])
+#         return dp[0][1]
+                
+#SPACE OPTIMISED TABULATION
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[0]*2 for _ in range(n+1)]
-
+        
+        prev=[0]*2
         for i in range(n-1,-1,-1):
-            dp[i][1]=max(-prices[i]+dp[i+1][0],dp[i+1][1])
-            dp[i][0]=max(prices[i]+dp[i+1][1],dp[i+1][0])
-        return dp[0][1]
+            curr=[0]*2
+            curr[1]=max(-prices[i]+prev[0],prev[1])
+            curr[0]=max(prices[i]+prev[1],prev[0])
+            prev=curr
+        return prev[1]
                 
-
 
