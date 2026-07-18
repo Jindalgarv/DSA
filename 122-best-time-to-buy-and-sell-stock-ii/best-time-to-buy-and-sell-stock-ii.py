0732a62a7e793ce66e34d6612052f1f8464dc1ec
Garv Jindal <garvjindal2@gmail.com>
2026-07-19 02:47:44 +0530

    Time: 47 ms (5.07%) | Memory: 32.2 MB (5.01%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,26 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        # profit=0
+        # for i in range(len(prices)-1):
+        #     if prices[i]<prices[i+1]:
+        #         profit+=prices[i+1]-prices[i]
+        # return profit
+
+#DP SOLUTION 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        dp=[[-1]*2 for _ in range(n)]
         profit=0
-        for i in range(len(prices)-1):
-            if prices[i]<prices[i+1]:
-                profit+=prices[i+1]-prices[i]
-        return profit
-        
+        def solve(i,buy):
+            if i==n:
+                return 0
+            if dp[i][buy]!=-1:
+                return dp[i][buy]
+            if buy:
+                profit=max(-prices[i]+solve(i+1,0),solve(i+1,1))
+            else:
+                profit=max(prices[i]+solve(i+1,1),solve(i+1,0))
+            dp[i][buy]=profit
+            return dp[i][buy]
+        return solve(0,1)
