73f2600a2faf60ac92db43659df3fb39b2812e09
Garv Jindal <garvjindal2@gmail.com>
2026-07-19 19:28:22 +0530

    Time: 785 ms (21.52%) | Memory: 62.5 MB (22.53%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,26 +1,52 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         n = len(prices)
+#         cap = 2
+
+#         dp = [[[-1] * (cap + 1) for _ in range(2)] for _ in range(n)]
+
+#         def solve(i, can_buy, cap):
+#             if i == n or cap == 0:
+#                 return 0
+
+#             if dp[i][can_buy][cap] != -1:
+#                 return dp[i][can_buy][cap]
+
+#             if can_buy:
+#                 buy = -prices[i] + solve(i + 1, 0, cap)
+#                 skip = solve(i + 1, 1, cap)
+#                 dp[i][can_buy][cap] = max(buy, skip)
+#             else:
+#                 sell = prices[i] + solve(i + 1, 1, cap - 1)
+#                 hold = solve(i + 1, 0, cap)
+#                 dp[i][can_buy][cap] = max(sell, hold)
+
+#             return dp[i][can_buy][cap]
+
+#         return solve(0, 1, cap)
+
+#TABULATION CODE
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         cap = 2
 
-        dp = [[[-1] * (cap + 1) for _ in range(2)] for _ in range(n)]
+        dp = [[[0] * (cap + 1) for _ in range(2)] for _ in range(n + 1)]
 
-        def solve(i, can_buy, cap):
-            if i == n or cap == 0:
-                return 0
+        for i in range(n - 1, -1, -1):
+            for can_buy in range(2):
+                for c in range(1, cap + 1):
+                    if can_buy:
+                        buy = -prices[i] + dp[i + 1][0][c]
+                        skip = dp[i + 1][1][c]
+                        dp[i][1][c] = max(buy, skip)
+                    else:
+                        sell = prices[i] + dp[i + 1][1][c - 1]
+                        hold = dp[i + 1][0][c]
+                        dp[i][0][c] = max(sell, hold)
 
-            if dp[i][can_buy][cap] != -1:
-                return dp[i][can_buy][cap]
+        return dp[0][1][cap]
 
-            if can_buy:
-                buy = -prices[i] + solve(i + 1, 0, cap)
-                skip = solve(i + 1, 1, cap)
-                dp[i][can_buy][cap] = max(buy, skip)
-            else:
-                sell = prices[i] + solve(i + 1, 1, cap - 1)
-                hold = solve(i + 1, 0, cap)
-                dp[i][can_buy][cap] = max(sell, hold)
 
-            return dp[i][can_buy][cap]
 
-        return solve(0, 1, cap)
