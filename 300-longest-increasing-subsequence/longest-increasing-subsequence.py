9f15ed8fc98120b4401ffd6392de49164e1674f7
Garv Jindal <garvjindal2@gmail.com>
2026-07-20 02:09:10 +0530

    Time: 1293 ms (23.24%) | Memory: 19.5 MB (31.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,16 +1,25 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # n=len(nums)
+        # dp=[[-1]*(n+1) for _ in range(n)]
+        # def solve(i,prev):
+        #     if i==n:
+        #         return 0
+        #     if dp[i][prev+1]!=-1:
+        #         return dp[i][prev+1]
+        #     not_take=solve(i+1,prev)
+        #     take=0
+        #     if prev== -1 or nums[i]>nums[prev]:
+        #         take=1+solve(i+1,i)
+        #     dp[i][prev+1] = max(take,not_take)
+        #     return dp[i][prev+1]
+        # return solve(0,-1)
+
+#TABULATION CODE
         n=len(nums)
-        dp=[[-1]*(n+1) for _ in range(n)]
-        def solve(i,prev):
-            if i==n:
-                return 0
-            if dp[i][prev+1]!=-1:
-                return dp[i][prev+1]
-            not_take=solve(i+1,prev)
-            take=0
-            if prev== -1 or nums[i]>nums[prev]:
-                take=1+solve(i+1,i)
-            dp[i][prev+1] = max(take,not_take)
-            return dp[i][prev+1]
-        return solve(0,-1)
+        dp=[0]*n
+        for i in range(n):
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    dp[i]=max(dp[i],dp[j]+1)
+        return 1+max(dp)
