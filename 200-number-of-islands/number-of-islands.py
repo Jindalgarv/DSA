1bed2c1646475cc3d4f5e77b1f41e15bed006400
Garv Jindal <garvjindal2@gmail.com>
2026-07-27 14:42:16 +0530

    Time: 247 ms (48.73%) | Memory: 22.2 MB (31.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        output=0
-        m,n=len(grid),len(grid[0])
-        visited=[[0]*n for _ in range(m)]
-        directions=((0,1),(1,0),(-1,0),(0,-1))
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        directions = [(-1,0), (1,0), (0,-1), (0,1)]
 
-        def dfs(i,j):
-            visited[i][j]=1
-            for dr,dc in directions:
-                nr,nc=i+dr,j+dc
-                if 0<=nr<m and 0<=nc<n and not visited[nr][nc] and grid[nr][nc]=='1':
-                    dfs(nr,nc)
+        def dfs(i, j):
+            visited[i][j] = True
 
+            for dr, dc in directions:
+                nr, nc = i + dr, j + dc
+                if (0 <= nr < m and
+                    0 <= nc < n and
+                    not visited[nr][nc] and
+                    grid[nr][nc] == "1"):
+                    dfs(nr, nc)
+
+        islands = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=='1' and not visited[i][j]:
-                    output+=1
-                    dfs(i,j)
-        return output
-                    
+                if not visited[i][j] and grid[i][j] == "1":
+                    dfs(i, j)
+                    islands += 1
 
-        
+        return islands
