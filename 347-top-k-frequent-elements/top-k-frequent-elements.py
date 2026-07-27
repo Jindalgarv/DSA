5a82656d87195d8a8c20d56e1f59a8e044d2daab
Garv Jindal <garvjindal2@gmail.com>
2026-07-28 00:21:12 +0530

    Time: 3 ms (89.70%) | Memory: 22.8 MB (60.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -12,3 +12,15 @@ class Solution:
             ans.append(num)
         return ans
 
+#SOLUTION 3 USING BUCKET SORT MOST OPTIMAL
+        freq = Counter(nums)
+        bucket=[[] for _ in range(len(nums)+1)]
+        for key,value in freq.items():
+            bucket[value].append(key)
+        res=[]
+        for i in range(len(bucket)-1,-1,-1):
+            for num in bucket[i]:
+                res.append(num)
+                if len(res)==k:
+                    return res
+
