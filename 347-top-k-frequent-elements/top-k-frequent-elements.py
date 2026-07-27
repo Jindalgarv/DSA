d211b9e31350ca23e912f6db587f5847a3b0dc2a
Garv Jindal <garvjindal2@gmail.com>
2026-07-28 00:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (32.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,36 +1,14 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #SOLUTION 1 NOT THE MOST OPTIMAL
-        # freq=Counter(nums)
-        # sorted_map=sorted(freq.items(),key= lambda x:x[1],reverse=True)
-        # res=[]
-        # return [key for key,value in sorted_map[0:k]]
+        heap=[]
+        count=Counter(nums)
+        for num,freq in count.items():
+            heappush(heap,(freq,num))
+            if len(heap)>k:
+                heappop(heap)
+        ans=[]
+        for freq,num in heap:
+            ans.append(num)
+        return ans
 
-        #SOLUTION 2 USING HEAP
-        # freq=Counter(nums)
-        # heap=[]
-        # for key, value in freq.items():
-        #     heappush(heap,(value,key))
-        #     if len(heap)>k:
-        #         heappop(heap)
-        # res=[]
-        # for value,key in heap:
-        #     res.append(key)
-        # return res
-
-#SOLUTION 3 USING BUCKET SORT
-        freq = Counter(nums)
-        bucket=[[] for _ in range(len(nums)+1)]
-        for key,value in freq.items():
-            bucket[value].append(key)
-        res=[]
-        for i in range(len(bucket)-1,-1,-1):
-            for num in bucket[i]:
-                res.append(num)
-                if len(res)==k:
-                    return res
-
-
-        
-        
