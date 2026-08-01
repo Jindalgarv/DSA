33b88860f4fc417c34587b4204b0fc4199d8cd22
Garv Jindal <garvjindal2@gmail.com>
2026-08-01 11:15:39 +0530

    Time: 235 ms (80.38%) | Memory: 22.2 MB (30.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         #IF WANT TO OPTIMIZE THE SPACE WE CAN JUST MODIFY THE GRID
-        m, n = len(grid), len(grid[0])
-        visited = [[False] * n for _ in range(m)]
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        m,n=len(grid),len(grid[0])
+        visited=[[0]*n for _ in range(m)]
+        directions=[(-1,0),(0,1),(1,0),(0,-1)]
+        def dfs(i,j):
+            visited[i][j]=1
+            for dr,dc in directions:
+                nr=i+dr
+                nc=j+dc
+                if 0<=nr<m and 0<=nc<n and not visited[nr][nc] and grid[nr][nc]=='1':
+                    dfs(nr,nc)
 
-        def dfs(i, j):
-            visited[i][j] = True
-
-            for dr, dc in directions:
-                nr, nc = i + dr, j + dc
-                if (0 <= nr < m and
-                    0 <= nc < n and
-                    not visited[nr][nc] and
-                    grid[nr][nc] == "1"):
-                    dfs(nr, nc)
-
-        islands = 0
+        count=0
         for i in range(m):
             for j in range(n):
-                if not visited[i][j] and grid[i][j] == "1":
-                    dfs(i, j)
-                    islands += 1
+                if not visited[i][j] and grid[i][j]=='1':
+                    dfs(i,j)
+                    count+=1
+        return count
+        
 
-        return islands
