a34bb9b09a7eb7adb9fc77a7b4d6232f17af404d
Garv Jindal <garvjindal2@gmail.com>
2026-08-04 10:30:35 +0530

    Time: 11 ms (43.47%) | Memory: 21 MB (48.29%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,7 +2,8 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n=len(height)
         water=0
-        preMax,sufMax=[height[0]]*n,[height[-1]]*n
+        preMax,sufMax=[0]*n,[0]*n
+        preMax[0],sufMax[-1]=height[0],height[-1]
         for i in range(1,n):
             preMax[i]=max(preMax[i-1],height[i])
         for i in range(n-2,-1,-1):
