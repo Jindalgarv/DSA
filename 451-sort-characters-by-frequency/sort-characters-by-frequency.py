01d2b01a5b26dc770f6c6b1a5f415b473ea50de4
Garv Jindal <garvjindal2@gmail.com>
2026-08-06 02:44:14 +0530

    Time: 7 ms (79.37%) | Memory: 20.2 MB (64.47%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,12 +1,13 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        freq=Counter(s)
-        arr=[]
-        res=[]
-        for key, value in freq.items():
-            arr.append((value,key))
-            arr.sort(reverse=True)
-        for n,ch in arr:
-            res.append(ch*n)
-        return "".join(res)
+        # freq=Counter(s)
+        # arr=[]
+        # res=[]
+        # for key, value in freq.items():
+        #     arr.append((value,key))
+        # arr.sort(reverse=True)
+        # for n,ch in arr:
+        #     res.append(ch*n)
+        # return "".join(res)
+        return "".join(ch*cnt for ch,cnt in Counter(s).most_common())
             
