07e58980421d57c5d7eb401026ddda1d77944324
Garv Jindal <garvjindal2@gmail.com>
2026-08-06 03:03:34 +0530

    Time: 12 ms (7.23%) | Memory: 19.3 MB (57.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,18 +1,31 @@
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         translations = {
+#             "I": 1,
+#             "V": 5,
+#             "X": 10,
+#             "L": 50,
+#             "C": 100,
+#             "D": 500,
+#             "M": 1000
+#         }
+#         number = 0
+#         s = s.replace("IV", "IIII").replace("IX", "VIIII")
+#         s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+#         s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+#         for char in s:
+#             number += translations[char]
+#         return number
 class Solution:
     def romanToInt(self, s: str) -> int:
-        translations = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
         }
-        number = 0
-        s = s.replace("IV", "IIII").replace("IX", "VIIII")
-        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
-        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
-        for char in s:
-            number += translations[char]
-        return number
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                result -= roman_to_int[s[i]]
+            else:
+                result += roman_to_int[s[i]]
+        return result
