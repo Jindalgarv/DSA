500d14337aeb7cc764eef5c0171717245be18ee4
Garv Jindal <garvjindal2@gmail.com>
2026-08-06 02:20:12 +0530

    Time: 4 ms (2.05%) | Memory: 19.3 MB (15.73%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -1,12 +1,16 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if s==goal:
-            return True
-        q1,q2=deque(s),deque(goal)
-        for i in range(len(q1)):
-            q1.appendleft(q1.pop())
-            if q1==q2:
-                return True
-        return False
+#NOT OPTIMAL BECAUSE COMPARING Q1 AND Q2 ALSO TAKES O(N) SO OVERALL IT IS N SQ
+
+        # if s==goal:
+        #     return True
+        # q1,q2=deque(s),deque(goal)
+        # for i in range(len(q1)):
+        #     q1.appendleft(q1.pop())
+        #     if q1==q2:
+        #         return True
+        # return False
+
+        return len(s) == len(goal) and goal in (s + s)
 
         
