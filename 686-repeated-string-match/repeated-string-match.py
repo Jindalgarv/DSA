dbdbc47728e0a9ab7ba20aa9b89821647f6143b6
Garv Jindal <garvjindal2@gmail.com>
2026-08-06 22:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.58%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.py
+++ 686-repeated-string-match/repeated-string-match.py
@@ -1,7 +1,7 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         n1,n2=len(a),len(b)
-        n=n2//n1+4
+        n=n2//n1+2
         c=""
         for i in range(n):
             c+=a
