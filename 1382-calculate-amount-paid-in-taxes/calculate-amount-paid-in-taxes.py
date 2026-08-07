3c709c2c7680d9a802cf2004b167a0375042fa4e
Garv Jindal <garvjindal2@gmail.com>
2026-08-08 00:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (66.61%) - LeetSync

--- 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.py
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.py
@@ -1,23 +1,15 @@
 class Solution:
     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
-        tax,remain=0,income
-        x=0
-        if remain>brackets[0][0]:
-            remain=remain-brackets[0][0]
-            tax+=brackets[0][0]*brackets[0][1]/100
-        else:
-            return remain*brackets[0][1]/100
-        i=0
-        while remain>0:
-            diff=brackets[i+1][0]-brackets[i][0]
-            if remain>diff:
-                remain-=diff
-                tax+=diff*brackets[i+1][1]/100
-                i+=1
-            else:
+        tax = 0.0
+        previous_limit = 0
+
+        for upper_limit, percent in brackets:
+            if income <= previous_limit:
                 break
-        tax+=remain*brackets[i+1][1]/100
+
+            taxable_income = min(income, upper_limit) - previous_limit
+            tax += taxable_income * percent / 100
+
+            previous_limit = upper_limit
+
         return tax
-
-
-        
