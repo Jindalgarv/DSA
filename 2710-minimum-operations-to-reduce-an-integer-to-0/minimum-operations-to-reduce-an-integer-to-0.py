b9b4080070779a7d0222fd3c4cc583bf78bd1abf
Garv Jindal <garvjindal2@gmail.com>
2026-08-17 01:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.30%) - LeetSync

---
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        ans=0
+        while n:
+            if n&1:
+                if n&2:
+                    n+=1
+                else:
+                    n-=1
+                ans+=1
+            else:
+                n>>=1
+        return ans
