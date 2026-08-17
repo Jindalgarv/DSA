f7939f0a6481966c319edb5aa43c1896f28b6982
Garv Jindal <garvjindal2@gmail.com>
2026-08-17 17:01:52 +0530

    Time: 15 ms (37.83%) | Memory: 19.5 MB (21.02%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.py
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.py
@@ -3,11 +3,11 @@ class Solution:
         n=len(word)
         ans=0
         for i in range(n):
-            if word[i] not in 'aeiou':
-                continue
+            seen=set()
             for j in range(i,n):
                 if word[j] not in 'aeiou':
                     break
-                if len(set(word[i:j+1]))==5:
+                seen.add(word[j])
+                if(len(seen))==5:
                     ans+=1
         return ans
