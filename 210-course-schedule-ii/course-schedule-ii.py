0e8b38061e2d2442c001b6818668a35848f6fb67
Garv Jindal <garvjindal2@gmail.com>
2026-08-17 12:20:53 +0530

    Time: 3 ms (74.65%) | Memory: 20.5 MB (55.15%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -3,29 +3,25 @@
 class Solution:
     def findOrder(
         self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        queue = deque()
+        q = deque()
         indegree = [0] * numCourses
         graph = [[] for _ in range(numCourses)]
         order = []
+        for u,v in prerequisites:
+            graph[v].append(u)
+            indegree[u]+=1
+        for node,deg in enumerate(indegree):
+            if deg==0:
+                q.append(node)
 
-        # To take course u, course v must be completed first:
-        # v -> u
-        for course, prerequisite in prerequisites:
-            graph[prerequisite].append(course)
-            indegree[course] += 1
-
-        for course in range(numCourses):
-            if indegree[course] == 0:
-                queue.append(course)
-
-        while queue:
-            node = queue.popleft()
+        while q:
+            node =q.popleft()
             order.append(node)
-
-            for neighbour in graph[node]:
-                indegree[neighbour] -= 1
-
-                if indegree[neighbour] == 0:
-                    queue.append(neighbour)
-
-        return order if len(order) == numCourses else []
+            for nei in graph[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        if len(order)==numCourses:
+            return order
+        return []
+                
