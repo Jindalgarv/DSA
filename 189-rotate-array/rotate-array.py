593c3eaaeae8a2f19d2aa17db2e59e49e1d42e6f
Garv Jindal <garvjindal2@gmail.com>
2026-08-18 17:54:34 +0530

    Time: 29 ms (15.44%) | Memory: 35.2 MB (6.05%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,7 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for _ in range(k):
-            num=nums.pop()
-            nums.insert(0,num)
-        
+        #SOLUTION 1 WITH O(1) SPACE
+        # for _ in range(k):
+        #     num=nums.pop()
+        #     nums.insert(0,num)
+
+#SOLUTION 2
+        # arr=[]
+        # for _ in range(k):
+        #     arr.append(nums.pop())
+            
+        # nums=arr+nums
+#OPTIMAL SOLUTION
+        n=len(nums)
+        k=k%n
+        nums.reverse()
+        nums[:k]=nums[:k][::-1]
+        nums[k:]=nums[k:][::-1]
