bb100960e743e61a49ffe58739feddff6c32150c
Garv Jindal <garvjindal2@gmail.com>
2026-09-08 20:57:36 +0530

    Time: 19 ms (19.77%) | Memory: 23.7 MB (30.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,11 +5,7 @@ class Solution:
             arr=[0]*26
             for ch in s:
                 arr[ord(ch)-ord('a')]+=1
-            t=tuple(arr)
-            d[t].append(s)
-        output=[]
-        for key,values in d.items():
-            output.append(values)
-        return output
+            d[tuple(arr)].append(s)
+        return list(d.values())
         
         
