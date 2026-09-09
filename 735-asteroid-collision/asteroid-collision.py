f9830143317af501e974a7dbeda85587b0a1939c
Garv Jindal <garvjindal2@gmail.com>
2026-09-09 14:16:24 +0530

    Time: 4 ms (76.24%) | Memory: 20.3 MB (70.63%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,18 +1,31 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack=[]
-        for x in asteroids:
-            if x>0:
-                stack.append(x)
+        for i in range(len(asteroids)-1,-1,-1):
+            if not stack or stack[-1]>0 or asteroids[i]<0:
+                stack.append(asteroids[i])
             else:
-                while(stack and stack[-1]>0):
-                    if(abs(x)>stack[-1]):
-                        stack.pop()
-                    elif(abs(x)==stack[-1]):
-                        stack.pop()
-                        break
-                    else:
-                        break
+                if abs(stack[-1])==abs(asteroids[i]):
+                    stack.pop()
+                    continue
+                if abs(stack[-1])>abs(asteroids[i]):
+                    continue
                 else:
-                    stack.append(x)
+                    while stack and stack[-1]<0 and abs(stack[-1])<abs(asteroids[i]):
+                        stack.pop()
+                    if stack and -stack[-1]==asteroids[i]:
+                        stack.pop()
+                        continue
+                    if not stack or stack[-1]>0:
+                        stack.append(asteroids[i])                    
+        stack.reverse()
         return stack
+
+        # stack=[]
+        # for x in asteroids:
+        #     if x>0:
+        #         stack.append()
+        #     else:
+        #         if stack[-1]<0:
+
+        
